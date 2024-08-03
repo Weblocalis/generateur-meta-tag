0562e0d7c5b93f49675d6904d3fc765ecbf11530
Mharzi <115441651+AbSphere@users.noreply.github.com>
2024-08-03 17:45:02 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,34 @@
-(function ($) {
+$(function () {
+    var $window = $(window),
+        $body = $('body'),
+        $nav = $('#nav'),
+        $navPanel = $('<div id="navPanel">').append($nav.html()).append('<a href="#navPanel" class="close"></a>');
 
-	// Breakpoints.
-	skel.breakpoints({
-		xlarge: '(max-width: 1680px)',
-		large: '(max-width: 1280px)',
-		medium: '(max-width: 980px)',
-		small: '(max-width: 736px)',
-		xsmall: '(max-width: 480px)'
-	});
+    $body.addClass('is-loading');
 
-	$(function () {
+    $window.on('load', function () {
+        window.setTimeout(function () {
+            $body.removeClass('is-loading');
+        }, 100);
+    });
 
-		var $window = $(window),
-			$body = $('body');
+    skel.on('+medium -medium', function () {
+        $.prioritize(
+            '.important\\28 medium\\29',
+            skel.breakpoint('medium').active
+        );
+    });
 
-		// Disable animations/transitions until the page has loaded.
-		$body.addClass('is-loading');
+    $navPanel.appendTo($body).panel({
+        delay: 500,
+        hideOnClick: true,
+        hideOnSwipe: true,
+        resetScroll: true,
+        resetForms: true,
+        side: 'left'
+    });
 
-		$window.on('load', function () {
-			window.setTimeout(function () {
-				$body.removeClass('is-loading');
-			}, 100);
-		});
-
-		// Prioritize "important" elements on medium.
-		skel.on('+medium -medium', function () {
-			$.prioritize(
-				'.important\\28 medium\\29',
-				skel.breakpoint('medium').active
-			);
-		});
-
-		// Off-Canvas Navigation.
-
-		// Navigation Panel.
-		$(
-			'<div id="navPanel">' +
-			$('#nav').html() +
-			'<a href="#navPanel" class="close"></a>' +
-			'</div>'
-		)
-			.appendTo($body)
-			.panel({
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				side: 'left'
-			});
-
-		// Fix: Remove transitions on WP<10 (poor/buggy performance).
-		if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-			$('#navPanel')
-				.css('transition', 'none');
-
-	});
-
-})(jQuery);
+    if (skel.vars.os === 'wp' && skel.vars.osVersion < 10) {
+        $navPanel.css('transition', 'none');
+    }
+});
