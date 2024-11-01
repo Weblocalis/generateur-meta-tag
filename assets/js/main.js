190f2f0fc23372872119a4cc1c9458b0b7628be1
Mharzi <115441651+AbSphere@users.noreply.github.com>
2024-11-01 19:29:53 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(function () {
-    $('.nav-toggle').on('click', function (e) {
-        e.preventDefault();
-        $('#nav').toggleClass('show'); // Toggle the 'show' class to open/close the menu
+    var $navPanel = $('#navPanel'); // Sélectionne le panneau de navigation mobile
+    var $navToggle = $('.nav-toggle'); // Sélectionne le bouton pour ouvrir le panneau
+
+    // Ouvre le panneau de navigation mobile
+    $navToggle.on('click', function (e) {
+        e.preventDefault(); // Empêche le comportement par défaut du lien
+        $navPanel.fadeIn(300); // Affiche le panneau avec une animation
+    });
+
+    // Ferme le panneau de navigation mobile lorsque le bouton de fermeture est cliqué
+    $navPanel.find('.close').on('click', function (e) {
+        e.preventDefault(); // Empêche le comportement par défaut du lien
+        $navPanel.fadeOut(300); // Cache le panneau avec une animation
     });
 });
