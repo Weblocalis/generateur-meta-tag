2f2a05ee0638046f4b083f3f5d9092bb9d717193
Mharzi <115441651+AbSphere@users.noreply.github.com>
2024-11-04 20:51:54 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,24 @@
 $(document).ready(function () {
-    var $navPanel = $('#navPanel'); // Sélectionne le panneau mobile
-    var $navToggle = $('.navPanelToggle'); // Sélectionne le bouton pour ouvrir le panneau
+    var $navPanel = $('#navPanel'); // Sélection du panneau mobile
+    var $navToggle = $('.navPanelToggle'); // Sélection du bouton pour ouvrir le panneau
 
-    // Ouvrir le panneau mobile au clic sur le bouton
+    // Ouvrir/fermer le panneau mobile au clic sur le bouton
     $navToggle.on('click', function (e) {
         e.preventDefault();
-        $navPanel.attr('aria-hidden', 'false').fadeIn(300); // Afficher le panneau avec une animation
+        if ($navPanel.attr('aria-hidden') === 'true') {
+            $navPanel.attr('aria-hidden', 'false'); // Rendre le panneau visible
+        } else {
+            $navPanel.attr('aria-hidden', 'true'); // Masquer le panneau
+        }
     });
 
     // Fermer le panneau mobile au clic sur "Fermer"
     $navPanel.find('.close').on('click', function (e) {
         e.preventDefault();
-        $navPanel.attr('aria-hidden', 'true').fadeOut(300); // Masquer le panneau avec une animation
+        $navPanel.attr('aria-hidden', 'true');
     });
 });
+
 
 // Affichage dynamique de l'année
 document.getElementById("currentYear").textContent = new Date().getFullYear();
