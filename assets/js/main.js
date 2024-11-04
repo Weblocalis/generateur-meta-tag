013cd28a7e91ce648dccf080b355750c64c8b462
Mharzi <115441651+AbSphere@users.noreply.github.com>
2024-11-04 20:39:23 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,15 @@ $(document).ready(function () {
     // Ouvrir le panneau mobile au clic sur le bouton
     $navToggle.on('click', function (e) {
         e.preventDefault();
-        $navPanel.fadeIn(300); // Afficher le panneau avec une animation
+        $navPanel.attr('aria-hidden', 'false').fadeIn(300); // Afficher le panneau avec une animation
     });
 
     // Fermer le panneau mobile au clic sur "Fermer"
     $navPanel.find('.close').on('click', function (e) {
         e.preventDefault();
-        $navPanel.fadeOut(300); // Masquer le panneau avec une animation
+        $navPanel.attr('aria-hidden', 'true').fadeOut(300); // Masquer le panneau avec une animation
     });
 });
 
-
+// Affichage dynamique de l'année
 document.getElementById("currentYear").textContent = new Date().getFullYear();
