0b6c12032ac214f372b72076e7ffdb4f2ceba28e
Mharzi <115441651+AbSphere@users.noreply.github.com>
2024-11-07 22:41:10 +0100

    co js

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,3 @@
-// to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
-
-getYear();
-
-// nice select
-$(document).ready(function () {
-    $('select').niceSelect();
-});
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     // Charger le fichier JSON contenant les villes
     fetch('https://weblocalis.github.io/boostifyseo-directory/data/villes.json')
@@ -40,11 +23,17 @@ document.addEventListener('DOMContentLoaded', function() {
             selectElement.appendChild(allLocationsOption);
 
             if (Array.isArray(data.cities)) {
+                const uniqueCities = new Map();
+                
                 data.cities.forEach(city => {
-                    const option = document.createElement('option');
-                    option.value = city.id;
-                    option.textContent = city.name;
-                    selectElement.appendChild(option);
+                    if (!uniqueCities.has(city.id)) {
+                        uniqueCities.set(city.id, city.name);
+                        
+                        const option = document.createElement('option');
+                        option.value = city.id;
+                        option.textContent = city.name;
+                        selectElement.appendChild(option);
+                    }
                 });
             } else {
                 console.error('Le fichier JSON ne contient pas de tableau "cities".');
@@ -53,4 +42,20 @@ document.addEventListener('DOMContentLoaded', function() {
         .catch(error => {
             console.error('Erreur lors du chargement des données JSON:', error);
         });
+});
+
+
+
+// to get current year
+function getYear() {
+    var currentDate = new Date();
+    var currentYear = currentDate.getFullYear();
+    document.querySelector("#displayYear").innerHTML = currentYear;
+}
+
+getYear();
+
+// nice select
+$(document).ready(function () {
+    $('select').niceSelect();
 });
